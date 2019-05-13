edca7d0cc187c815479273e8f258d5fe0bda60cc
Shola Ayeni <ayenisholah@yahoo.com>
2019-05-13 13:53:24 +0100

    Challenge 3: Object Methods Completed

--- assignments/objects.js
+++ assignments/objects.js
@@ -54,8 +54,19 @@ console.log(interns[4].gender)
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+interns[1].speak = function sayHello() {
+  console.log("Hello, my name is Kennan");
+}
+
+console.log (interns[1].speak())
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+interns[4].multiplyNums = function multiply(x,y){
+  return x*y;
+}
+console.log(interns[4].multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
